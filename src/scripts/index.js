31082d460685e31c7fe48399f77572555b6ac4e8
catarinaccmateus <catarina_1102@hotmail.com>
2020-01-15 14:56:48 +0000

    set up with parcelJS, handlebars and scss

---
+++ src/scripts/index.js
@@ -0,0 +1,21 @@
+import "./../styles/style.scss";
+
+function getMobileMenu() {
+  console.log("clicked on menu");
+  /*
+  var x = document.getElementById("menu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }*/
+}
+
+document.getElementById("navbar-icon").onclick = function() {
+  var x = document.getElementById("menu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+};

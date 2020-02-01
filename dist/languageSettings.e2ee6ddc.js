bca24a4f1c9ac124199cc7df41f54dabbee13b1d
catarinaccmateus <catarina_1102@hotmail.com>
2020-02-01 11:53:51 +0000

    added english version of site in javascript

--- dist/languageSettings.e2ee6ddc.js
+++ dist/languageSettings.e2ee6ddc.js
@@ -117,222 +117,83 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../../../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
-
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
-  }
-
-  return bundleURL;
-}
-
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
-  }
-
-  return '/';
-}
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"../../../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
-  };
-
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-
-var cssTimeout = null;
-
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
-
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
-
-    cssTimeout = null;
-  }, 50);
-}
-
-module.exports = reloadCSS;
-},{"./bundle-url":"../../../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"../../styles/style.scss":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"_css_loader":"../../../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../scripts/index.js":[function(require,module,exports) {
+})({"../../scripts/content.js":[function(require,module,exports) {
 "use strict";
 
-require("./../styles/style.scss");
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.pt = exports.en = void 0;
+var en = {
+  navAbout: 'About',
+  navServices: 'Services',
+  navPartners: 'Partnerships',
+  navContacts: 'Contacts',
+  aboutAbout: "About us",
+  aboutDescription: "'FM Consultores Associados' was formed on May 25, 1990. <br> Its founder, Fernando Mateus, has a degree in Organization and Business Management, and he is registered in the Order of Certified Accountants since August 4, 1987 under number 23776. He is a member of the Governing Bodies of several private companies, he started his professional activity leading the Accounting Services of the Portuguese Industrial Association.",
+  clientsClients: 'Our clients',
+  clientsDescription: "Since its foundation, 'FM Consultores Associados' has always grown and nowadays it has a large portfolio of customers from different sectors.",
+  partnersPartnerships: 'Partnerships',
+  servicesServices: 'Services',
+  servicesAccountant: 'Accounting',
+  servicesTax: 'Tax Calculation',
+  servicesClientsManagement: 'Clients Management',
+  servicesConsulting: 'Consulting',
+  servicesHumanResources: 'Human Resources',
+  contactsContacts: 'Contacts',
+  contactsAddress: 'Address'
+};
+exports.en = en;
+var pt = {
+  navAbout: 'Sobre nós',
+  navServices: 'Serviços',
+  navPartners: 'Parcerias',
+  navContacts: 'Contactos',
+  aboutAbout: 'Sobre nós',
+  aboutDescription: "A “FM Consultores Associados” foi constituída em 25 de Maio de 1990. <br> O seu fundador Fernando Mateus é licenciado em Organização e Gestão de Empresa, inscrito da ordem dos Contabilistas Certificados desde 4 de agosto de 1987 com o numero 23776. É membro de Órgãos Sociais de diversas empresas privadas tendo iniciado a sua atividade profissional chefiando os Serviços de Contabilidade da Associação Industrial Portuguesa.",
+  clientsClients: 'Os nossos clientes',
+  clientsDescription: 'Desde da sua fundação veio sempre a crescer e conta com uma grande carteira de clientes de diferentes setores.',
+  partnersPartnerships: 'Parcerias',
+  servicesServices: 'Serviços',
+  servicesAccountant: 'Contabilidade Organizada',
+  servicesTax: 'Apuramento de Impostos',
+  servicesClientsManagement: 'Gestão de Clientes',
+  servicesConsulting: 'Consultoria',
+  servicesHumanResources: 'Recursos Humanos',
+  contactsContacts: 'Contactos',
+  contactsAddress: 'Morada'
+};
+exports.pt = pt;
+},{}],"../../scripts/languageSettings.js":[function(require,module,exports) {
+"use strict";
 
-//To show/hide hamburger menu from navbar
-document.getElementById("navbar-icon").onclick = function () {
-  var x = document.getElementById("menu");
-  var y = document.getElementById("navbar-icon");
+var _content = require("./content");
 
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    y.innerHTML = "<div></div><div></div><div></div>";
-  } else {
-    x.style.display = "block";
-    y.innerHTML = "<div class=first-line></div><div class=second-line></div>";
+document.getElementById('enSelect').addEventListener('click', englishWebsite);
+document.getElementById('ptSelect').addEventListener('click', portuguesehWebsite);
+
+function englishWebsite() {
+  var enKeys = Object.keys(_content.en);
+
+  for (var _i = 0, _enKeys = enKeys; _i < _enKeys.length; _i++) {
+    var key = _enKeys[_i];
+    document.getElementById(key).innerHTML = _content.en[key];
   }
-}; //To hide menu when clicking on window or menu links;
-
-
-var menuLinks = document.getElementsByClassName('navbar-link');
-
-for (var i = 0; i < menuLinks.length; i++) {
-  menuLinks[i].addEventListener("click", hideMenu);
 }
 
-document.getElementById('siteContent').addEventListener("click", hideMenu);
+;
 
-function hideMenu() {
-  var x = document.getElementById("menu");
-  var y = document.getElementById("navbar-icon");
+function portuguesehWebsite() {
+  var ptKeys = Object.keys(_content.pt);
 
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    y.innerHTML = "<div></div><div></div><div></div>";
+  for (var _i2 = 0, _ptKeys = ptKeys; _i2 < _ptKeys.length; _i2++) {
+    var key = _ptKeys[_i2];
+    document.getElementById(key).innerHTML = _content.pt[key];
   }
-} //To add less opacity in navbar when scrolling down
-
-
-var scrollpos = window.scrollY;
-var navbar = document.getElementById("nav");
-
-function add_class_on_scroll() {
-  navbar.classList.add("lessOpacity");
 }
 
-function remove_class_on_scroll() {
-  navbar.classList.remove("lessOpacity");
-}
-
-window.addEventListener("scroll", function () {
-  scrollpos = window.scrollY;
-
-  if (scrollpos > 20) {
-    add_class_on_scroll();
-  } else {
-    remove_class_on_scroll();
-  }
-}); //Client logos caroussel
-
-var slideIndex = 0;
-var clientsCarousel = setInterval(showSlides, 1500);
-
-function showSlides() {
-  var slides = document.getElementsByClassName("slides");
-
-  for (var _i = 0; _i < slides.length; _i++) {
-    slides[_i].style.display = "none";
-  }
-
-  slideIndex++;
-
-  if (slideIndex > slides.length - 2) {
-    slideIndex = 1;
-  }
-
-  if (slideIndex <= slides.length - 2) {
-    slides[slideIndex - 1].style.display = "block";
-    slides[slideIndex].style.display = "block";
-    slides[slideIndex + 1].style.display = "block";
-  } //setTimeout(showSlides, 500);
-
-} // PREVIOUS ATTEMPTS
-// 1.
-//This code is an alternative to the above. Instead of changing the display of the images, I would change the src of the three images.
-// This will make them all the time displayable and wouldn't make the window to jump up when scrolling down.
-// Due to the bundler from parcelJS changing the images folders and names, it was not possible to use this solution.
-// const arrayOfClientImages = [
-//   "atelier38.png",
-//   "ateliermedions.png",
-//   "b.leza.jpg",
-//   "contemporanea.png",
-//   "CVDB.jpg"
-// ];
-// let imageIndex = 0;
-// let clientsCarousel = setInterval(showImages, 500);
-// function showImages() {
-//   let firstImage = document.getElementById("firstImage");
-//   let secondImage = document.getElementById("secondImage");
-//   let thirdImage = document.getElementById("thirdImage");
-//   if (imageIndex < arrayOfClientImages.length - 3) {
-//     imageIndex++;
-//   } else {
-//     imageIndex = 0;
-//   }
-//   firstImage.alt = arrayOfClientImages[imageIndex];
-//   secondImage.alt = arrayOfClientImages[imageIndex + 1];
-//   thirdImage.alt = arrayOfClientImages[imageIndex + 2];
-//   firstImage.src = `./../../images/clients/${arrayOfClientImages[imageIndex]}`;
-//   secondImage.src = `./../../images/clients/${
-//     arrayOfClientImages[imageIndex + 1]
-//   }`;
-//   thirdImage.src = `./../../images/clients/${
-//     arrayOfClientImages[imageIndex + 2]
-//   }`;
-// }
-
-/* 
-2.
-The following code is if I wanted to have a carousel of images in the first container. I opted to take this option out since some images
-// may take some time to load what could compromise the first impression when accessing the site.
-
-//Title caroussel testing
-
-showSlides();
-
-let numberOfImage = 1;
-let classNameOfImage = `background-image-1`;
-
-function showSlides() {
-const titleContainter = document.getElementById('title');
-titleContainter.classList.remove(classNameOfImage);
-if (numberOfImage <4) {
-  numberOfImage++;
-} else {
-  numberOfImage = 1;
-}
-console.log(classNameOfImage);
-classNameOfImage = `background-image-${numberOfImage}`
-titleContainter.classList.add(classNameOfImage);
-  setTimeout(showSlides, 1000); 
-}
-*/
-},{"./../styles/style.scss":"../../styles/style.scss"}],"../../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+;
+},{"./content":"../../scripts/content.js"}],"../../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -536,5 +397,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","../../scripts/index.js"], null)
-//# sourceMappingURL=/scripts.fe2889fe.js.map
+},{}]},{},["../../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","../../scripts/languageSettings.js"], null)
+//# sourceMappingURL=/languageSettings.e2ee6ddc.js.map
---
+++ src/scripts/languageSettings.js
@@ -0,0 +1,20 @@
+import {en, pt} from './content';
+
+document.getElementById('enSelect').addEventListener('click', englishWebsite);
+
+document.getElementById('ptSelect').addEventListener('click', portuguesehWebsite);
+
+function englishWebsite () {
+  const enKeys = Object.keys(en);
+  for (const key of enKeys) {
+    document.getElementById(key).innerHTML = en[key];
+  }
+};
+
+function portuguesehWebsite () {
+  const ptKeys = Object.keys(pt);
+  for (const key of ptKeys) {
+    document.getElementById(key).innerHTML = pt[key];
+  }
+};
+
